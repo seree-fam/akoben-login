d1c9301e1db6cdcc95cf52fd3ed336cd828ee7e0
lancenonce <lancendavis47@gmail.com>
2024-06-18 20:24:34 +0000

    return logic

--- src/SemaphoreButton.tsx
+++ src/SemaphoreButton.tsx
@@ -2,6 +2,16 @@ import React, { useState } from 'react';
 
 const SemaphoreButton: React.FC = () => {
 
+    return (
+        <div>
+          {!active ? (
+            <button onClick={}>Connect Wallet</button>
+          ) : (
+            <button onClick={}>Sign In with Semaphore</button>
+          )}
+          {isAuthenticated && <p>Welcome to the site!</p>}
+        </div>
+      );
 }
 
 export default SemaphoreButton;
