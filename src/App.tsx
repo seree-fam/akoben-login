d527560f7796402380db11397ec990f07e447e03
lancenonce <lancendavis47@gmail.com>
2024-06-18 20:51:42 +0000

    add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import SemaphoreButton from './SemaphoreButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getLibrary(provider: any): Web3Provider {
+  return new Web3Provider(provider);
 }
+
+const App: React.FC = () => {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <header className="App-header">
+          <SemaphoreButton />
+        </header>
+      </div>
+    </Web3ReactProvider>
+  );
+};
 
 export default App;
